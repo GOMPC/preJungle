4c15e1971ed3801b713557dfbe25785674b562bd
KINGPC <crossbag108@naver.com>
2022-09-17 16:59:23 +0900

    Jungle End?

--- Chap3/pymongoEx4.py
+++ Chap3/pymongoEx4.py
@@ -2,3 +2,9 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.dbjungle 
 
+target_movie = db.movies.find_one({'title':'매트릭스'})
+print(target_movie['star'])
+
+for movie in db.movies:
+    if movie['star'] == target_movie['star']:
+        print(movie['title'])
